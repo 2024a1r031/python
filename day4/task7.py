fca10c3586a9bf0162ead5071f903db432538804
Radhika031 <2024a1r031@mietjammu.in>
2026-09-01 13:10:04 +0530

    Update task7.py

--- day4/task7.py
+++ day4/task7.py
@@ -2,6 +2,6 @@
 name = input("Enter student name: ")
 roll_no = input("Enter roll number: ")
 
-username = name[0] + name[1] + name[2] + roll_no[-2] + roll_no[-1]
-
+# username = name[0] + name[1] + name[2] + roll_no[-2] + roll_no[-1]
+username = name[:3] + roll_no[-2:]
 print("Username:", username)
